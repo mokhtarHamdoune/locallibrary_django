516c7235db5ff558364c3da04992bf7ef9812079
mokhtarHamdoune <mokhtardark@gmail.com>
2021-04-26 01:43:08 +0100

    simple use of sessions

--- catalog/views/index.py
+++ catalog/views/index.py
@@ -14,6 +14,17 @@ def index(request):
     num_authors = Author.objects.count()
 
     hp_books = Book.objects.filter(title__icontains='harry').count()
+    # get the value of the 'num_visits' session key if does not exsit set it to 1
+    num_visits = request.session.get('num_visits',1)
+    # increment the session key 
+    request.session['num_visits'] = num_visits + 1
+
+    #if the key session is an object like a case then 
+    # django with not recognize the changes if you made a change 
+    # so you have to explicitly mark the session as having been modified.
+    # Set session as modified to force data updates/cookie to be saved.
+    
+    # request.session.modified = True
 
     #context to send to the tamplate
     context = {
@@ -21,7 +32,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'hp_books': hp_books
+        'hp_books': hp_books,
+        'num_visits':num_visits
     }
 
     return render(request, 'index.html', context=context)
